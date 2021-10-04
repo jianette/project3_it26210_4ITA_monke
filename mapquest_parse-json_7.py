e0511d97f173e8e6c290dc396d0ec4476574b148
jianette <60497499+jianette@users.noreply.github.com>
2021-10-04 12:47:19 +0800

    Add files via upload

--- mapquest_parse-json_7.py
+++ mapquest_parse-json_7.py
@@ -18,12 +18,34 @@ while True:
         print("API Status: " + str(json_status) + " = A successful route call.\n")
         print("=============================================")
         print("Directions from " + (orig) + " to " + (dest))
+        #Statement for the Toll Gate
+        if json_data["route"]["hasTollRoad"] == True:
+            print("The road that you will go through will have a toll gate so ready your cash.")
+        else:
+            print("The road that you will go through will not have a toll gate.")
+        #Statement for the Bridge
+        if json_data["route"]["hasBridge"] == True:
+            print("The road that you will go through will have a bridge so be safe when driving.")
+        else:
+            print("The road that you will go through will not have a bridge.")
+        #Statement for the Tunnel
+        if json_data["route"]["hasTunnel"] == True:
+            print("The road that you will go through will have a tunnel so make sure your lights are working.")
+        else:
+            print("The road that you will go through will not have a tunnel.")
+        #Statement for the Cross Country
+        if json_data["route"]["hasCountryCross"] == True:
+            print("You need to go to another country to reach your destination.")
+        else:
+            print("You don't have to go to another country to reach your destination.")
+
         print("Trip Duration: " + (json_data["route"]["formattedTime"]))
         print("Kilometers: " + str("{:.2f}".format((json_data["route"]["distance"])*1.61)))
         print("Fuel Used (Ltr): " + str("{:.2f}".format((json_data["route"]["fuelUsed"])*3.78)))
         print("=============================================")
+        #Added a time in each legs of the route
         for each in json_data["route"]["legs"][0]["maneuvers"]:
-            print((each["narrative"]) + " (" + str("{:.2f}".format((each["distance"])*1.61) + " km)"))
+            print((each["narrative"]) + " With a distance of " + str("{:.2f}".format((each["distance"])*1.61) + " km") + " and estimated time of "+ (each["formattedTime"]))
         print("=============================================\n")
     elif json_status == 402:
         print("**********************************************")
